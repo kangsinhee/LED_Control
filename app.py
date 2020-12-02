21c525b5ba721fcd4dff32c291a5f21b586259eb
kangsinhee <52666197+kangsinhee@users.noreply.github.com>
2020-12-02 10:48:07 +0900

    clean up app.py

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import  RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
-status = 0
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.OUT)
+def gpio_init():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(14, GPIO.OUT)
+    print('Web Server starts')
 
+def controller(status):
+    print(status)
+    GPIO.output(14, status)
+
+@app.route('/')
+def index():
+    return redirect(url_for('off'))
 
 @app.route('/OFF')
 def off():
     status = 0
-    print(status)
-    GPIO.output(14, 1)
-    return render_template('OFF.html', status= status)
+    controller(status)
+    return render_template('OFF.html', status=status)
 
 @app.route('/ON')
 def on():
     status = 1
-    print(status)
-    GPIO.output(14, 0)
+    controller(status)
     return render_template('ON.html', status=status)
 
 @app.route('/about')
@@ -27,8 +33,8 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    print('Web Server starts')
-    app.run(debug = True, host="127.0.0.3", port="8888")
+    gpio_init()
+    app.run(debug = True)
 
 GPIO.cleanup()
 print('Program End')
